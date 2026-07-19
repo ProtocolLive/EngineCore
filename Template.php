acb3233e82bbc05c7ad1be70697cad70b8b0c80b
Protocol Corporation <protocollive@outlook.com>
2026-07-19 19:27:06 -0300

    Mensagem de erro corrigida

--- Template.php
+++ Template.php
@@ -5,7 +5,7 @@ namespace ProtocolLive\SuperLauda\Core;
 use DomainException;
 
 /**
- * @version 2026.07.04.00
+ * @version 2026.07.19.00
  */
 final class Template{
   private string $Template;
@@ -36,7 +36,7 @@ final class Template{
     if($Root):
       $file = $this->Dir . '/' . $this->Template . '/' . $File;
       if(is_file($file) === false):
-        error_log('Ftemplate file ' . $file . ' not found');
+        error_log('Template file ' . $file . ' not found');
         return false;
       endif;
       require $file;
